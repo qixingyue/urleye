b4e3b816f6589ea474ed0a4a1a352973276f42bc
xingyue <qixingyue@126.com>
2017-03-02 10:50:00 +0800

    格式缩进修改

--- eye.py
+++ eye.py
@@ -1,100 +1,15 @@
 #!/data0/python27/bin/python
 #coding=utf-8
 
-import requests
-import handlers
-import fetcher
-import conf
-import datetime
-import lib.log 
+import lib.core
 import sys
-
-sql = """
-CREATE TABLE `url_alarm` (
-id int primary key auto_increment,
-check_url varchar(128) not null default "",
-actions varchar(127) not null default "",
-minutes int not null default 1,
-hours int not null default 0,
-date int not null default 0,
-ctime datetime default CURRENT_TIMESTAMP,
-mtime datetime default CURRENT_TIMESTAMP
-)
-"""
-
-
-def parse_watch(str):
-
-    w = {}
-    arr = str.split(" ")
-    if len(arr) != 5 :
-        print str
-        print "parse item error !"
-        exit(1)
-
-    w["minute"] = arr[0]
-    w["hour"] = arr[1]
-    w["day"] = arr[2]
-    w["url"] = arr[3]
-    w["actions"] = arr[4]
-
-    return w
-
-def do_watch(item):
-    if "url" in item and "actions" in item:
-        if item["url"][0:4] == "http" :
-            response = requests.get(item['url'])
-            if response.status_code == 200 :
-                content = response.text
-
-        elif item["url"][0:5] == "fetch":
-            f_items = item["url"][8:].split(".")
-            if len(f_items) != 0 :
-                fetcher_name = f_items.pop(0) 
-                params = f_items
-                content = fetcher.fetch(fetcher_name,params)
-            else :
-                content = False
-
-        if False != content:
-            names = item['actions'].split(",")
-            for n in names:
-                handlers.call(n,content)
-
-    else :
-        print "ITEM ERROR!"
-
-def should_run(now,w):
-
-    if int(w["minute"]) == -1 :
-        return True
-
-    flags = [True,True,True]
-
-    flags[0] = (w["day"] == "*") or (int(w["day"]) == now.day)
-    flags[1] = (w["hour"] == "*") or (int(w["hour"]) == now.hour)
-    flags[2] = (w["minute"] == "*") or (int(w["minute"]) == now.minute)
-
-    return flags[0] and flags[1] and flags[2]
-
-
-def do_once():
-
-    lib.log.log("RUN BEGIN :")
-    now = datetime.datetime.now()
-
-    watches = conf.url.watches
-    for item in watches:
-        watch = parse_watch(item)
-        if should_run(now,watch):
-            do_watch(watch)
 
 if __name__ == "__main__":
 
     i = len(sys.argv)
     if i > 1:
         arr = sys.argv[1].split(" ")
-        do_watch({"url":arr[0],"actions":arr[1]})
+        lib.core.do_watch({"url":arr[0],"actions":arr[1]})
         exit()
-    do_once()
+    lib.core.do_once()
 
--- handlers/wrapper.py
+++ handlers/wrapper.py
@@ -4,12 +4,12 @@
 handlers = {}
 
 def hander_method(fn):
-	global handlers
-	handlers[fn.__name__] = fn
+    global handlers
+    handlers[fn.__name__] = fn
 
 def call(name,content):
-	if name in handlers:
-		handlers[name](content)
+    if name in handlers:
+        handlers[name](content)
     else :
         print "No handler named : %s " % (name)
 
