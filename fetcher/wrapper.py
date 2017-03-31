abc8eb51481f690e398b17ec894cad31b52b2dfd
xingyue <qixingyue@126.com>
2017-03-31 15:24:54 +0800

    log 的error 输出

--- fetcher/wrapper.py
+++ fetcher/wrapper.py
@@ -4,16 +4,16 @@ import lib.log
 
 fetchers = {}
 
-def fetch_method(fn):
-    global fetchers 
-    fetchers[fn.__name__] = fn
+def fetch_object(cla):
+    global fetchers
+    fetchers[cla.name] = cla
 
-def fetch(name,params):
+def scheme_get(name,url):
     global fetchers
     if name in fetchers:
-        return fetchers[name](params)
+        return fetchers[name]().scheme_get(url)
     else :
-        lib.log.log("No fetcher named %s " % (name))
+        lib.log.error("No scheme named %s " % (name))
         return False
 
 import baidu
--- lib/log.py
+++ lib/log.py
@@ -7,13 +7,19 @@ def log(message):
 	now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 	print "%s%s%s %s " % (color.green,now,color.end ,message)
 
+def error(message):
+	now = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+	print "%s%s %s %s " % (color.red,now,message,color.end)
+
 def info():
     print """
 *** %sUrleys%s Start Ok.
 *** This is One framework run progreames based on urls and actions.
 *** You can define some urls in conf/url.py like "minute hour url acions"
-*** actions can be implode by "," like demo,redis or more 
-*** that means that time will run get content from that url , then handler these contents by handlers defined .
-""" % (color.green,color.end)
+*** Actions can be implode by "," like demo,redis or more 
+*** That means that time will run get content from that url 
+*** Then handler these contents by handlers defined .
+*** Simple Run : %s python eye.py "fetch://baidu.com demo" %s
+""" % (color.green,color.end,color.green,color.end)
 
 
